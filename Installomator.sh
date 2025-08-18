195e1da3c9b33890d9c1682b442d45ca83e9d9ab
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2025-08-18 15:28:01 +0200

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -10526,15 +10526,6 @@ yubicoauthenticator)
 yubikeymanager)
     name="YubiKey Manager"
     type="pkg"
-    appCustomVersion(){/usr/local/ykman/ykman -v | awk '{print $5}'}
-	downloadURL=$(downloadURLFromGit Yubico yubikey-manager)
-    appNewVersion=$(versionFromGit Yubico yubikey-manager)
-    expectedTeamID="LQA3CS5MM7"
-    #CLI for YubikeyManager which is not installed via the QT version.
-    ;;
-yubikeymanagerqt)
-    name="YubiKey Manager GUI"
-    type="pkg"
     downloadURL="https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-latest-mac.pkg"
     appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | grep -Eo "[1-9][0-9\.]*" )
     expectedTeamID="LQA3CS5MM7"
