d1be36c3088e7ab1eaf15845870d7d21e6944171
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2025-08-18 15:28:36 +0200

    Delete fragments/labels/yubikeymanagerqt.sh

--- fragments/labels/yubikeymanagerqt.sh
+++
@@ -1,7 +0,0 @@
-yubikeymanagerqt)
-    name="YubiKey Manager GUI"
-    type="pkg"
-    downloadURL="https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-latest-mac.pkg"
-    appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i "^location" | grep -Eo "[1-9][0-9\.]*" )
-    expectedTeamID="LQA3CS5MM7"
-    ;;
