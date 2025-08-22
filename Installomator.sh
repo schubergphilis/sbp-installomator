eaba35b8491b119193aaaebe74b999a2ba1f3c00
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2025-08-22 11:36:34 +0200

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -348,8 +348,8 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
         rosetta2=no
     fi
 fi
-VERSION="12.8"
-VERSIONDATE="2025-08-18"
+VERSION="12.9"
+VERSIONDATE="2025-08-22"
 
 # MARK: Functions
 
