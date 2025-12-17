802a02ae6bb39db148ff1de3351d9650053ba608
Gerard Spierenburg <42139030+gspierenburg-sbp@users.noreply.github.com>
2025-12-17 15:03:44 +0100

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -4492,7 +4492,11 @@ firefoxpkg_intl)
 flexoptixapp)
     name="FLEXOPTIX App"
     type="dmg"
-    downloadURL="https://flexbox.reconfigure.me/download/electron/mac/x64/current"
+    if [[ $(/usr/bin/arch) == "arm64" ]]; then
+        downloadURL="https://flexbox.reconfigure.me/download/electron/mac/arm64/current"
+    else
+        downloadURL="https://flexbox.reconfigure.me/download/electron/mac/x64/current"
+    fi
     appNewVersion=$(curl -fsIL "${downloadURL}" | grep -i ^location | sed -E 's/.*App-(.*)\.dmg/\1/g')
     expectedTeamID="C5JETSFPHL"
     ;;
